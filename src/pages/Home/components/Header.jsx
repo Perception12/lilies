4b11cd5d7ff76878c9bf9ae719c3fbe29516c623
Kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2022-11-27 13:41:57 +0100

    add screen breakpoints

--- src/pages/Home/components/Header.jsx
+++ src/pages/Home/components/Header.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav class="container relative mx-auto p-6">
+      <div class="flex items-center justify-between">
+        <div class='flex items-center space-x-2'>
+          <img src="../../../assets/img/logo.png" alt="" />
+        </div>
+      </div>
+    </nav>
   )
 }
 
