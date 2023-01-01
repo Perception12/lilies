6c15e9d9a1b3a0ed7519a7e1b329fc178b79f992
Kehinde Adejumobi <kehindeadejumobi82@gmail.com>
2023-01-01 14:00:40 +0100

    fixing bug

---
+++ src/assets/index.js
@@ -0,0 +1,17 @@
+import appStore from "./img/appStore.png";
+import burger from "./img/burger.png";
+import googlePlay from "./img/googlePlay.png"
+import heroImg from "./img/hero-img.png"
+import logo from "./img/logo.png"
+import meatballs from "./img/meatballs.png"
+import stirfry from "./img/stir-fry.png"
+
+export {
+    appStore,
+    burger,
+    googlePlay,
+    heroImg,
+    logo,
+    meatballs,
+    stirfry,
+}
---
+++ src/constants/index.js
@@ -0,0 +1,58 @@
+import {} from "../assets"
+
+export const navLinks = [
+    {
+        id: "home",
+        title: "Home",
+        link: "/",
+    },
+    {
+        id: "login",
+        title: "Login",
+        link: "/auth/login"
+    },
+    {
+        id: "register",
+        title: "Sign Up",
+        link: "/auth/register"
+    },
+]
+
+export const mealsData = [
+    {
+        id: "stirfry",
+        name: "Stir fry Pasta",
+        shortDescription: "The in-house pasta and chicken by chef Moose",
+        longDescription: 'Just have a single bite of this Black Forest pastry and it will all make a proper sense to you. The kick of cherry and rich chocolate of this super light, airy pastry will definitely make you feel "wow". The perfect combination of cherry cream and rich chocolate can provide the ultimate fulfillment to your dessert craving.'
+    },
+    {
+        id: "stirfry",
+        name: "Stir fry Pasta",
+        shortDescription: "The in-house pasta and chicken by chef Moose",
+        longDescription: 'Just have a single bite of this Black Forest pastry and it will all make a proper sense to you. The kick of cherry and rich chocolate of this super light, airy pastry will definitely make you feel "wow". The perfect combination of cherry cream and rich chocolate can provide the ultimate fulfillment to your dessert craving.'
+    },
+    {
+        id: "stirfry",
+        name: "Stir fry Pasta",
+        shortDescription: "The in-house pasta and chicken by chef Moose",
+        longDescription: 'Just have a single bite of this Black Forest pastry and it will all make a proper sense to you. The kick of cherry and rich chocolate of this super light, airy pastry will definitely make you feel "wow". The perfect combination of cherry cream and rich chocolate can provide the ultimate fulfillment to your dessert craving.'
+    },
+    {
+        id: "stirfry",
+        name: "Stir fry Pasta",
+        shortDescription: "The in-house pasta and chicken by chef Moose",
+        longDescription: 'Just have a single bite of this Black Forest pastry and it will all make a proper sense to you. The kick of cherry and rich chocolate of this super light, airy pastry will definitely make you feel "wow". The perfect combination of cherry cream and rich chocolate can provide the ultimate fulfillment to your dessert craving.'
+    },
+    {
+        id: "stirfry",
+        name: "Stir fry Pasta",
+        shortDescription: "The in-house pasta and chicken by chef Moose",
+        longDescription: 'Just have a single bite of this Black Forest pastry and it will all make a proper sense to you. The kick of cherry and rich chocolate of this super light, airy pastry will definitely make you feel "wow". The perfect combination of cherry cream and rich chocolate can provide the ultimate fulfillment to your dessert craving.'
+    },
+    {
+        id: "stirfry",
+        name: "Stir fry Pasta",
+        shortDescription: "The in-house pasta and chicken by chef Moose",
+        longDescription: 'Just have a single bite of this Black Forest pastry and it will all make a proper sense to you. The kick of cherry and rich chocolate of this super light, airy pastry will definitely make you feel "wow". The perfect combination of cherry cream and rich chocolate can provide the ultimate fulfillment to your dessert craving.'
+    },
+]
